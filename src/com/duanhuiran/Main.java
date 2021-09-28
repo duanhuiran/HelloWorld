8e2c6a29a4a543d0f92fe1a968d9a619c179f016
DuanHuiran <570989207@qq.com>
2021-09-28 15:11:15 +0200

    The Twelve Days of Christmas repository commit and push 03

--- src/com/duanhuiran/Main.java
+++ src/com/duanhuiran/Main.java
@@ -2,8 +2,120 @@ package com.duanhuiran;
 
 public class Main {
 
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("On the first day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "A partridge in a pear tree.\n" +
+                "\n" +
+                "On the second day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the third day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the fourth day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the fifth day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Five golden rings,\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the sixth day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Six geese a-laying,\n" +
+                "Five golden rings,\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the seventh day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Seven swans a-swimming,\n" +
+                "Six geese a-laying,\n" +
+                "Five golden rings,\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the eighth day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Eight maids a-milking,\n" +
+                "Seven swans a-swimming,\n" +
+                "Six geese a-laying,\n" +
+                "Five golden rings,\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the ninth day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Nine ladies dancing,\n" +
+                "Eight maids a-milking,\n" +
+                "Seven swans a-swimming,\n" +
+                "Six geese a-laying,\n" +
+                "Five golden rings,\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the tenth day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Ten lords a-leaping,\n" +
+                "Nine ladies dancing,\n" +
+                "Eight maids a-milking,\n" +
+                "Seven swans a-swimming,\n" +
+                "Six geese a-laying,\n" +
+                "Five golden rings,\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the eleventh day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Eleven pipers piping,\n" +
+                "Ten lords a-leaping,\n" +
+                "Nine ladies dancing,\n" +
+                "Eight maids a-milking,\n" +
+                "Seven swans a-swimming,\n" +
+                "Six geese a-laying,\n" +
+                "Five golden rings,\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree.\n" +
+                "\n" +
+                "On the twelfth day of Christmas,\n" +
+                "my true love sent to me\n" +
+                "Twelve drummers drumming,\n" +
+                "Eleven pipers piping,\n" +
+                "Ten lords a-leaping,\n" +
+                "Nine ladies dancing,\n" +
+                "Eight maids a-milking,\n" +
+                "Seven swans a-swimming,\n" +
+                "Six geese a-laying,\n" +
+                "Five golden rings,\n" +
+                "Four calling birds,\n" +
+                "Three French hens,\n" +
+                "Two turtle doves,\n" +
+                "And a partridge in a pear tree!");
     }
 }
